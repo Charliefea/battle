b827f823e1de5556248a97dce1602081d07a685a
Panteha <panteha.ahmadi@gmail.com>
2017-05-25 14:57:43 +0100

    Solved

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -13,3 +13,11 @@ end
       expect(page).to have_content('Geoff score: 5')
     end
   end
+
+  feature 'Attack player and get confirmation' do
+    scenario 'Player 1 attacks player 2' do
+      sign_in_and_play
+      click_button "Attack!"
+      expect(page).to have_content('Sally attacked Geoff')
+    end
+  end
