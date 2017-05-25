e0bece45a3ec8f6e33c3f520aec0af0314b746a2
Panteha <panteha.ahmadi@gmail.com>
2017-05-25 12:25:38 +0100

    Added and tested score for player 2

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -9,3 +9,13 @@ feature 'Start a fight' do
     expect(page).to have_content("Sally vs Geoff")
   end
 end
+
+  feature 'See a hit point' do
+    scenario 'After starting the battle, player 1 sees player 2 hit points' do
+      visit '/'
+      fill_in "player1", :with => "Sally"
+      fill_in "player2", :with => "Geoff"
+      click_button "Fight!"
+      expect(page).to have_content('Geoff score: 5')
+    end
+  end
