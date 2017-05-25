ab268bf74f17e252adf196bcf0e6bf69225bcee8
Panteha <panteha.ahmadi@gmail.com>
2017-05-25 15:39:35 +0100

    Added global variable, created player class, removed session/tested

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,9 @@ require 'player.rb'
 
 describe Player do
   let(:player) {described_class.new('Sally')}
+
   it 'returns the name of the player' do
+    puts player
     expect(player.name).to eq 'Sally'
   end
 
